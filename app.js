d95dbd2f172f8ef8123fd0636c7fd07b2a96459c
Ben <meteoben@gmail.com>
2022-03-30 11:13:47 +0200

    améliorez server.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) =>{
+    console.log('requête reçue');
+    next();
+});
 
-app.use((req, res) =>{
-    res.json({ message :'Votre requête a bien été reçue !' })
+app.use((req, res, next) =>{
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) =>{
+    res.json({ message :'Votre requête a bien été reçue !' });
+    next();
+})
+app.use((req, res) => {
+    console.log('Reponse envoyée avec succès');
 })
 module.exports = app;
